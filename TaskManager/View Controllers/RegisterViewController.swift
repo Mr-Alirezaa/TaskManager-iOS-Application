796282e6b0eaee1938d80d2b36ff7ca0caeeff04
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-05-18 21:48:36 +0430

    Added files

---
+++ TaskManager/View Controllers/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  TaskManager
+//
+//  Created by Alireza Asadi on 28/2/1398 AP.
+//  Copyright © 1398 AP MrAlirezaa. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TaskManager/Views/TMTextField.swift
@@ -0,0 +1,14 @@
+//
+//  TMTextField.swift
+//  TaskManager
+//
+//  Created by Alireza Asadi on 28/2/1398 AP.
+//  Copyright © 1398 AP MrAlirezaa. All rights reserved.
+//
+
+import UIKit
+
+class TMTextField: UITextField {
+
+
+}
