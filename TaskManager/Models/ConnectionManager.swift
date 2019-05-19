5290541d556ba07529a69008fcacf8ab45deb27a
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-05-19 14:12:39 +0430

    ConnectionManager added + storyboard minor changes

---
+++ TaskManager/Models/ConnectionManager.swift
@@ -0,0 +1,13 @@
+//
+//  ConnectionManager.swift
+//  TaskManager
+//
+//  Created by Alireza Asadi on 29/2/1398 AP.
+//  Copyright © 1398 AP MrAlirezaa. All rights reserved.
+//
+
+import Foundation
+
+struct ConnectionManager {
+    
+}
