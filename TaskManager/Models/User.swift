36ee6bb11e978d84af471fc48ed8baec1521643a
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-06-06 14:15:21 +0430

    User struct added.

---
+++ TaskManager/Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  TaskManager
+//
+//  Created by Alireza Asadi on 16/3/1398 AP.
+//  Copyright © 1398 AP MrAlirezaa. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    var firstName: String?
+    var lastName: String?
+    var email: String?
+    var password: String?
+    var phoneNumber: String?
+}
