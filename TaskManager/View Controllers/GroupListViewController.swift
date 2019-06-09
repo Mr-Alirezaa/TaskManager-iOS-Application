72638bc0f65c2528d6d3e6a1b9a103fd056deb72
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-06-09 16:08:31 +0430

    GroupListViewController added.

---
+++ TaskManager/View Controllers/GroupListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GroupListViewController.swift
+//  TaskManager
+//
+//  Created by Alireza Asadi on 19/3/1398 AP.
+//  Copyright © 1398 AP MrAlirezaa. All rights reserved.
+//
+
+import UIKit
+
+class GroupListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
