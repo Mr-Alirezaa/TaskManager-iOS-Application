1bafc5ac4a80ec4c1ca54b2c0b2b62b0cbab9cb6
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-06-09 16:00:19 +0430

    Login and Back features work.

--- TaskManager/View Controllers/LoginViewController.swift
+++ TaskManager/View Controllers/LoginViewController.swift
@@ -10,12 +10,42 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    private let connectionManager = ConnectionManager.default
+    
+    @IBOutlet weak var emailTextField: TMTextField!
+    @IBOutlet weak var passwordTextField: TMTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = true
     }
     
     @IBAction func backButton(_ sender: UIButton) {
-        self.navigationController?.dismiss(animated: true, completion: nil)
+        print("----> back button tapped")
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func loginButtonTap(_ sender: UIButton) {
+        print("textField: --- email: \(emailTextField.text!), pass: \(passwordTextField.text!)")
+//        var resultUser: User?
+        connectionManager.login(username: emailTextField.text!, password: passwordTextField.text!) { [weak self] (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .failure(let err):
+                    print(err)
+                case .success(let user):
+                    print(user)
+                    let nextViewController = self?.storyboard?.instantiateViewController(withIdentifier: "firstAfterLogin")
+                    self?.navigationController?.pushViewController(nextViewController!, animated: true)
+                }
+            }
+        }
+//        print("Before return")
+//        if resultUser != nil { return true }
+//        return false
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        return true
     }
 }
