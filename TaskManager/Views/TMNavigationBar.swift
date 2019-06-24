eead07900369107da03a6a169682e285d7a5f60b
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-06-24 17:45:01 +0430

    Navbar refined.

--- TaskManager/Views/TMNavigationBar.swift
+++ TaskManager/Views/TMNavigationBar.swift
@@ -22,12 +22,12 @@ class TMNavigationBar: UINavigationBar {
 
     private func setupNavigationBar() {
         let largeTitleAttributes: [NSAttributedString.Key : Any] = [.font : UIFont(name: TMFonts.shabnamBold, size: 36)!,
-                                                                    .foregroundColor : UIColor.white]
-        let normalTitleAttributes: [NSAttributedString.Key : Any] = [.font : UIFont(name: TMFonts.shabnamMedium, size: 17)!,
+                                                                    .foregroundColor : TMColors.lightBlue]
+        let normalTitleAttributes: [NSAttributedString.Key : Any] = [.font : UIFont(name: TMFonts.shabnamBold, size: 18)!,
                                                                      .foregroundColor : UIColor.white]
 
         if #available(iOS 11.0, *) {
-            self.prefersLargeTitles = true
+            self.prefersLargeTitles = false
         }
 
         self.semanticContentAttribute = .forceRightToLeft
@@ -39,27 +39,5 @@ class TMNavigationBar: UINavigationBar {
         self.largeTitleTextAttributes = largeTitleAttributes
         self.titleTextAttributes = normalTitleAttributes
 
-        let bgimage = imageWithGradient(startColor: UIColor.red, endColor: UIColor.yellow, size: CGSize(width: UIScreen.main.bounds.size.width, height: 1))
-        self.setBackgroundImage(bgimage, for: UIBarMetrics.default)
-    }
-
-    private func imageWithGradient(startColor:UIColor, endColor:UIColor, size:CGSize, horizontally:Bool = true) -> UIImage? {
-
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
-        if horizontally {
-            gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
-            gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
-        } else {
-            gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
-            gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
-        }
-
-        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
-        gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image
     }
 }
