995545bb8b2a7f56a2985b1d31011a78cad6cf00
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-06-24 17:51:42 +0430

    taskListTableViewCell added.

---
+++ TaskManager/Views/TaskListTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TaskListTableViewCell.swift
+//  TaskManager
+//
+//  Created by Alireza Asadi on 3/4/1398 AP.
+//  Copyright © 1398 AP MrAlirezaa. All rights reserved.
+//
+
+import UIKit
+
+class TaskListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var taskTitleLabel: UILabel!
+    @IBOutlet weak var checkButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func checkButtonTapped(_ sender: UIButton) {
+
+
+    }
+}
