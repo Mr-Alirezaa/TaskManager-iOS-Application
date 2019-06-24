d4c63435939cb5676ae26595a217d92320dee849
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-06-24 17:37:19 +0430

    TaskListViewController added.

---
+++ TaskManager/View Controllers/TaskListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListViewController.swift
+//  TaskManager
+//
+//  Created by Alireza Asadi on 3/4/1398 AP.
+//  Copyright © 1398 AP MrAlirezaa. All rights reserved.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
