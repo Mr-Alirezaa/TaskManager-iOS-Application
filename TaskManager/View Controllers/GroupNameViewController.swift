0672186ebde3153df3242857a5ddf5adfced2f48
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-06-24 17:40:06 +0430

    submit button functioning.

--- TaskManager/View Controllers/GroupNameViewController.swift
+++ TaskManager/View Controllers/GroupNameViewController.swift
@@ -9,25 +9,59 @@
 import UIKit
 
 class GroupNameViewController: UIViewController {
+    
+    let connectionManager = ConnectionManager.default
 
     @IBOutlet weak var instructionLabel: UILabel!
     @IBOutlet weak var groupNameTextField: TMTextField!
     
-    var initialGroupName: String?
-    var isEditingMode: Bool = true
+    var taskGroup: TMTaskGroup?
+    var isEditingMode: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let name = initialGroupName {
+        if isEditingMode {
             instructionLabel.text = "نام جدید را انتخاب کنید."
-            groupNameTextField.text = name
+            groupNameTextField.text = taskGroup?.name
         }
     }
     @IBAction func submitButtonTapped(_ sender: UIButton) {
-        
+        if groupNameTextField.hasText {
+            if isEditingMode, let group = taskGroup {
+                if group.name != groupNameTextField.text {
+                    connectionManager.editTaskGroupName(taskGroup: group, to: groupNameTextField.text!) { (result) in
+                        DispatchQueue.main.async {
+                            switch result {
+                            case .failure(_):
+                                break
+                            case .success(_):
+                                self.dismiss(animated: true, completion: nil)
+                            }
+                        }
+                    }
+                }
+            } else {
+                connectionManager.createTaskGroup(withName: groupNameTextField.text!) { (result) in
+                    DispatchQueue.main.async {
+                        switch result {
+                        case .failure(_):
+                            break
+                        case .success(_):
+                            self.navigationController?.popViewController(animated: true)
+                        }
+                    }
+                }
+            }
+        } else {
+            let alert = UIAlertController(title: "خطا در انتخاب اسم", message: "لطفا برای گروه یک اسم انتخال کنید.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "متوجه شدم", style: .default, handler: nil)
+            alert.addAction(okAction)
+            
+            self.present(alert, animated: true, completion: nil)
+        }
     }
     @IBAction func backButtonTapped(_ sender: UIButton) {
-        
+        self.navigationController?.dismiss(animated: true, completion: nil)
     }
 }
