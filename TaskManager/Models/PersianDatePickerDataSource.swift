30ec4ebd1949cbc5bdf3660a029bd53377cd8de8
Alireza Asadi <alireza.asadi.36@gmail.com>
2019-07-06 23:24:54 +0430

    persian datepicker data source added.

---
+++ TaskManager/Models/PersianDatePickerDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  PersianDatePicker.swift
+//  TaskManager
+//
+//  Created by Alireza Asadi on 15/4/1398 AP.
+//  Copyright © 1398 AP MrAlirezaa. All rights reserved.
+//
+
+import Foundation
+
+public class PersianDatePickerDataSource {
+    
+}
---
+++ TaskManager/View Controllers/NewTaskViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewTaskViewController.swift
+//  TaskManager
+//
+//  Created by Alireza Asadi on 15/4/1398 AP.
+//  Copyright © 1398 AP MrAlirezaa. All rights reserved.
+//
+
+import UIKit
+
+class NewTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
